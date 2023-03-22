a8a7ae823340f543f94589c88289ad1d294c432c
rfujioka <rodrigofujioka@gmail.com>
2023-03-22 18:04:26 -0300

    Atualização de crud para turma de SPI Unipes

--- src/main/java/br/com/fujideia/iesp/tecback/model/Filme.java
+++ src/main/java/br/com/fujideia/iesp/tecback/model/Filme.java
@@ -4,19 +4,19 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @Entity
 @Table(name = "tb_filme")
-public class Filme {
+public class Filme implements Serializable {
 
     @Id
-    private int id;
+    @GeneratedValue
+    private Integer id;
     private String titulo;
     @Column(name = "ds_sinopse", length = 500)
     private String sinopse;
-
-    @Transient
-    private String teste;
 
 }
