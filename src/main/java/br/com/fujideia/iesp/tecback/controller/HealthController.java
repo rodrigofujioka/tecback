e34e827091ab4089794d6dae67813a5ab2372928
rfujioka <rodrigofujioka@gmail.com>
2024-02-27 20:18:24 -0300

    Atualização do código para aula de socialização com github e SpringDoc

---
+++ src/main/java/br/com/fujideia/iesp/tecback/controller/HealthController.java
@@ -0,0 +1,17 @@
+package br.com.fujideia.iesp.tecback.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public String health(){
+        return "OK";
+    }
+
+}
