ee2eac945f0c9738002d685bf8469ca37cfb06f8
edgarneto12 <edgarneto789@gmail.com>
2024-04-16 20:41:27 -0300

    update

--- src/main/java/br/com/fujideia/iesp/tecback/TecbackApplication.java
+++ src/main/java/br/com/fujideia/iesp/tecback/TecbackApplication.java
@@ -1,13 +1,14 @@
 package br.com.fujideia.iesp.tecback;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 @SpringBootApplication
 @EnableFeignClients
 public class TecbackApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TecbackApplication.class, args);
 	}
--- src/main/java/br/com/fujideia/iesp/tecback/controller/CepController.java
+++ src/main/java/br/com/fujideia/iesp/tecback/controller/CepController.java
@@ -1,4 +1,22 @@
 package br.com.fujideia.iesp.tecback.controller;
 
+import br.com.fujideia.iesp.tecback.clients.ViaCepClient;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/cep")
 public class CepController {
+
+    private ViaCepClient viaCepClient;
+
+    @GetMapping("/{cep}")
+    public String consultaCep(@PathVariable String cep) {
+        return viaCepClient.consultaEndereco(cep);
+    }
+
 }
