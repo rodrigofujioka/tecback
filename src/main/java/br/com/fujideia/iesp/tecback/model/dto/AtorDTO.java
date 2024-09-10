34fa07f16bd969483b109579def7a53c0ef38150
rfujioka <rodrigofujioka@gmail.com>
2024-09-10 18:34:14 -0300

    Inclusão de material de aula para turma em 10/09/2024

---
+++ src/main/java/br/com/fujideia/iesp/tecback/model/dto/AtorDTO.java
@@ -0,0 +1,4 @@
+package br.com.fujideia.iesp.tecback.model.dto;
+
+public class AtorDTO {
+}
--- src/main/java/br/com/fujideia/iesp/tecback/service/FilmeService.java
+++ src/main/java/br/com/fujideia/iesp/tecback/service/FilmeService.java
@@ -1,12 +1,20 @@
 package br.com.fujideia.iesp.tecback.service;
 
+import br.com.fujideia.iesp.tecback.model.Ator;
+import br.com.fujideia.iesp.tecback.model.Diretor;
 import br.com.fujideia.iesp.tecback.model.Filme;
+import br.com.fujideia.iesp.tecback.model.Genero;
+import br.com.fujideia.iesp.tecback.model.dto.AtorDTO;
+import br.com.fujideia.iesp.tecback.model.dto.DiretorDTO;
+import br.com.fujideia.iesp.tecback.model.dto.FilmeDTO;
+import br.com.fujideia.iesp.tecback.model.dto.GeneroDTO;
 import br.com.fujideia.iesp.tecback.repository.FilmeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -14,32 +22,32 @@ public class FilmeService {
 
     private final FilmeRepository filmeRepository;
 
-    public List<Filme> listarTodos() {
-        return filmeRepository.findAll();
+    public List<FilmeDTO> listarTodos() {
+        return filmeRepository.findAll()
+                .stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
     }
 
-    public Optional<Filme> buscarPorId(Long id) {
-        return filmeRepository.findById(id);
+    public Optional<FilmeDTO> buscarPorId(Long id) {
+        return filmeRepository.findById(id)
+                .map(this::convertToDTO);
     }
 
-    public Filme criarFilme(Filme filme) {
-        return filmeRepository.save(filme);
+    public FilmeDTO criarFilme(FilmeDTO filmeDTO) {
+        Filme filme = convertToEntity(filmeDTO);
+        return convertToDTO(filmeRepository.save(filme));
     }
 
-    public Optional<Filme> atualizarFilme(Long id, Filme filmeDetalhes) {
-        Optional<Filme> filmeOptional = filmeRepository.findById(id);
-        if (filmeOptional.isPresent()) {
-            Filme filme = filmeOptional.get();
-            filme.setTitulo(filmeDetalhes.getTitulo());
-            filme.setAnoLancamento(filmeDetalhes.getAnoLancamento());
-            filme.setDiretor(filmeDetalhes.getDiretor());
-            filme.setAtores(filmeDetalhes.getAtores());
-            filme.setGeneros(filmeDetalhes.getGeneros());
-            Filme filmeAtualizado = filmeRepository.save(filme);
-            return Optional.of(filmeAtualizado);
-        } else {
-            return Optional.empty();
-        }
+    public Optional<FilmeDTO> atualizarFilme(Long id, FilmeDTO filmeDTO) {
+        return filmeRepository.findById(id).map(filme -> {
+            filme.setTitulo(filmeDTO.getTitulo());
+            filme.setAnoLancamento(filmeDTO.getAnoLancamento());
+            filme.setDiretor(convertToEntity(filmeDTO.getDiretor()));
+            filme.setAtores(filmeDTO.getAtores().stream().map(this::convertToEntity).collect(Collectors.toList()));
+            filme.setGeneros(filmeDTO.getGeneros().stream().map(this::convertToEntity).collect(Collectors.toList()));
+            return convertToDTO(filmeRepository.save(filme));
+        });
     }
 
     public boolean deletarFilme(Long id) {
@@ -48,5 +56,47 @@ public class FilmeService {
             return true;
         }
         return false;
+    }
+
+    private FilmeDTO convertToDTO(Filme filme) {
+        return new FilmeDTO(
+                filme.getId(),
+                filme.getTitulo(),
+                filme.getAnoLancamento(),
+                new DiretorDTO(filme.getDiretor().getId(), filme.getDiretor().getNome()),
+                filme.getAtores().stream().map(ator -> new AtorDTO(ator.getId(), ator.getNome())).collect(Collectors.toList()),
+                filme.getGeneros().stream().map(genero -> new GeneroDTO(genero.getId(), genero.getDescricao())).collect(Collectors.toList())
+        );
+    }
+
+    private Filme convertToEntity(FilmeDTO filmeDTO) {
+        Filme filme = new Filme();
+        filme.setTitulo(filmeDTO.getTitulo());
+        filme.setAnoLancamento(filmeDTO.getAnoLancamento());
+        filme.setDiretor(convertToEntity(filmeDTO.getDiretor()));
+        filme.setAtores(filmeDTO.getAtores().stream().map(this::convertToEntity).collect(Collectors.toList()));
+        filme.setGeneros(filmeDTO.getGeneros().stream().map(this::convertToEntity).collect(Collectors.toList()));
+        return filme;
+    }
+
+    private Diretor convertToEntity(DiretorDTO diretorDTO) {
+        Diretor diretor = new Diretor();
+        diretor.setId(diretorDTO.getId());
+        diretor.setNome(diretorDTO.getNome());
+        return diretor;
+    }
+
+    private Ator convertToEntity(AtorDTO atorDTO) {
+        Ator ator = new Ator();
+        ator.setId(atorDTO.getId());
+        ator.setNome(atorDTO.getNome());
+        return ator;
+    }
+
+    private Genero convertToEntity(GeneroDTO generoDTO) {
+        Genero genero = new Genero();
+        genero.setId(generoDTO.getId());
+        genero.setDescricao(generoDTO.getDescricao());
+        return genero;
     }
 }
