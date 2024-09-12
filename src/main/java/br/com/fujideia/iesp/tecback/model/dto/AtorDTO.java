edcfaf5e87dcb5609bd981906fa7a69c55a5dcb2
rfujioka <rodrigofujioka@gmail.com>
2024-09-12 04:52:43 -0300

    Inclusão de conteúdo turma de tecnologias de backEnd 1 Uniesp

--- src/main/java/br/com/fujideia/iesp/tecback/model/dto/AtorDTO.java
+++ src/main/java/br/com/fujideia/iesp/tecback/model/dto/AtorDTO.java
@@ -1,4 +1,13 @@
 package br.com.fujideia.iesp.tecback.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AtorDTO {
+    private Long id;
+    private String nome;
 }
--- src/main/java/br/com/fujideia/iesp/tecback/model/dto/FilmeDTO.java
+++ src/main/java/br/com/fujideia/iesp/tecback/model/dto/FilmeDTO.java
@@ -1,4 +1,20 @@
 package br.com.fujideia.iesp.tecback.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FilmeDTO {
+    private Long id;
+    private String titulo;
+    private int anoLancamento;
+    private DiretorDTO diretor;
+    private List<AtorDTO> atores;
+    private List<GeneroDTO> generos;
 }
+
