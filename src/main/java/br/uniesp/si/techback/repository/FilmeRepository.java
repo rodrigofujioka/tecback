7b0d85fa3a50ac66973f33f6f40c891f6c61a96f
rfujioka <rodrigofujioka@gmail.com>
2025-04-15 18:17:21 -0300

    Inclusão de queries de apoio

--- src/main/java/br/uniesp/si/techback/repository/FilmeRepository.java
+++ src/main/java/br/uniesp/si/techback/repository/FilmeRepository.java
@@ -2,8 +2,40 @@ package br.uniesp.si.techback.repository;
 
 import br.uniesp.si.techback.model.Filme;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FilmeRepository extends JpaRepository<Filme, Integer> {
+
+    public Filme findByTitulo(String titulo);
+    public Filme findByAutor(String autor);
+    public Filme findByTituloAndAutor(String titulo, String autor);
+    public List<Filme> findFilmeByTituloIsLikeIgnoreCase(String titulo);
+    public List<Filme> findFilmeByAutorIsLikeIgnoreCase(String autor);
+    public List<Filme> findFilmeByTituloContainingIgnoreCase(String titulo);
+
+    @Query("SELECT f FROM Filme f WHERE f.titulo = :titulo")
+    Filme buscarPorTitulo(@Param("titulo") String titulo);
+
+    @Query("SELECT f FROM Filme f WHERE f.autor = :autor")
+    Filme buscarPorAutor(@Param("autor") String autor);
+
+    @Query("SELECT f FROM Filme f WHERE f.titulo = :titulo AND f.autor = :autor")
+    Filme buscarPorTituloEAutor(@Param("titulo") String titulo, @Param("autor") String autor);
+
+    @Query("SELECT f FROM Filme f WHERE LOWER(f.titulo) LIKE LOWER(CONCAT('%', :titulo, '%'))")
+    List<Filme> buscarPorTituloLikeIgnoreCase(@Param("titulo") String titulo);
+
+    @Query("SELECT f FROM Filme f WHERE LOWER(f.autor) LIKE LOWER(CONCAT('%', :autor, '%'))")
+    List<Filme> buscarPorAutorLikeIgnoreCase(@Param("autor") String autor);
+
+    @Query("SELECT f FROM Filme f WHERE LOWER(f.titulo) LIKE LOWER(CONCAT('%', :titulo, '%'))")
+    List<Filme> buscarFilmesComTituloContendo(@Param("titulo") String titulo);
+
+
+
 }
