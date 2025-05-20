3d589dd909b2ab5e7c44a76f6ed3e0d6690e7d30
rfujioka <rodrigofujioka@gmail.com>
2025-05-20 14:55:54 -0300

    Atualizando dados para turma

--- src/main/java/br/uniesp/si/techback/controller/FilmeController.java
+++ src/main/java/br/uniesp/si/techback/controller/FilmeController.java
@@ -2,20 +2,26 @@ package br.uniesp.si.techback.controller;
 
 import br.uniesp.si.techback.model.Filme;
 import br.uniesp.si.techback.service.FilmeService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/filmes")
 public class FilmeController {
 
-    @Autowired
-    private FilmeService service;
+
+    private final FilmeService service;
 
     @GetMapping
+    @Cacheable("listar_filmes_all")
     public List<Filme> listarTodos() {
         return service.listarTodos();
     }
@@ -34,10 +40,12 @@ public class FilmeController {
 
     @PutMapping("/{id}")
     public ResponseEntity<Filme> atualizar(@PathVariable Integer id, @RequestBody Filme filme) {
+        log.info("Atualizando filme {}", filme);
         try {
             Filme atualizado = service.atualizar(id, filme);
             return ResponseEntity.ok(atualizado);
         } catch (RuntimeException e) {
+            log.error("Erro ao atualizar filme {} erro {} ", filme, e);
             return ResponseEntity.notFound().build();
         }
     }
--- src/main/java/br/uniesp/si/techback/service/FilmeService.java
+++ src/main/java/br/uniesp/si/techback/service/FilmeService.java
@@ -2,23 +2,29 @@ package br.uniesp.si.techback.service;
 
 import br.uniesp.si.techback.model.Filme;
 import br.uniesp.si.techback.repository.FilmeRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
+@RequiredArgsConstructor
 @Service
 public class FilmeService {
 
-    @Autowired
-    private FilmeRepository repository;
+
+    private final FilmeRepository repository;
 
     public List<Filme> listarTodos() {
+        log.info("Listando todos os filmes");
         return repository.findAll();
     }
 
     public Optional<Filme> buscarPorId(Integer id) {
+        log.info("Busca de filmes por id {} ", id);
         return repository.findById(id);
     }
 
