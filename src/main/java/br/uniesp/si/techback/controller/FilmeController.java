45c19d95a2daff033d8250833dd4dad9ba4e0301
AlbertoLucas04 <abertolucas12@gmail.com>
2025-09-02 21:09:49 -0300

    Class commit - 09.02.2025

--- src/main/java/br/uniesp/si/techback/controller/FilmeController.java
+++ src/main/java/br/uniesp/si/techback/controller/FilmeController.java
@@ -1,11 +1,13 @@
 package br.uniesp.si.techback.controller;
 
+import br.uniesp.si.techback.model.Filme;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -14,5 +16,22 @@ import java.util.List;
 @RequestMapping("/filmes")
 public class FilmeController {
 
+    private List<Filme> lista = new ArrayList<>();
+    private long contador = 1;
+
+    @PostMapping
+    public Filme incluir(@RequestBody Filme filme){
+
+        filme.setId(contador);
+        ++contador;
+        lista.add(filme);
+        return filme;
+
+    }
+
+    @GetMapping
+    public List<Filme> listar(){
+        return lista;
+    }
 
 }
